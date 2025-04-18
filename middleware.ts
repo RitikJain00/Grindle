14b5a73d8da46508b4e97ace62d9d85c4cf1185a
Ritik <ritikjain590@gmail.com>
2025-04-18 19:29:33 +0530

    Added Landing page and Authorization

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isProtectedRoute =  createRouteMatcher([
   '/dashboard(.*)',
@@ -14,7 +15,9 @@ export default clerkMiddleware( async(auth, req) => {
   if(!userId && isProtectedRoute(req)){
     const { redirectToSignIn } = await auth();
     return redirectToSignIn()
-  }
+  } 
+
+  return NextResponse.next()
 });
 
 export const config = {
